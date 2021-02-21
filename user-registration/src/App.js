23bfe767ed51f18a14feec3cd89ec5625e1a569e
Eligijus <elasorama@gmail.com>
2021-02-21 18:12:50 +0200

    Added the API call page

--- user-registration/src/App.js
+++ user-registration/src/App.js
@@ -4,30 +4,62 @@ import React, { Component } from 'react';
 // Importing registration form component 
 import RegistrationForm from './components/RegistrationForm';
 
+// Importing test API call component 
+import APIcall from './components/APIcall';
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 class App extends Component {
 
   render() {
 
     return (
-      <div className='main-page'>
-        <header className='main-page-header'>
-          <p> Welcome to the user registration page </p>
-        </header>
+      <Tabs>
+        <TabList>
+          <Tab>Register users</Tab>
+          <Tab>API </Tab>
+        </TabList>
 
-        <div className='sub-header'>
-          <p>
-            The users registered through this page will be able to use the Heart Failure API.
+        <TabPanel>
+        <div className='main-page'>
+          <header className='main-page-header'>
+            <p> Welcome to the user registration page </p>
+          </header>
+
+          <div className='sub-header'>
+            <p>
+              The users registered through this page will be able to use the Heart Failure API.
           </p>
 
-          <p>
-            The main API project in GIT:
+            <p>
+              The main API project in GIT:
           <a href="https://github.com/Eligijus112/flask-application" target='_blank'> Heart failure API </a>
+            </p>
+          </div>
+          <div className='form'>
+            <RegistrationForm />
+          </div>
+        </div>
+        </TabPanel>
+
+        <TabPanel>
+        <div className='main-page'>
+          <header className='main-page-header'>
+            <p> Make an API call </p>
+          </header>
+
+          <div className='sub-header'>
+            <p>
+              Test out the API by mocking some data and seeing the results
           </p>
+
+          </div>
+          <div className='form'>
+            <APIcall/>
+          </div>
         </div>
-        <div className='form'>
-          <RegistrationForm />
-        </div>
-      </div>
+        </TabPanel>
+      </Tabs>
     )
   }
 }
---
+++ user-registration/src/components/APIcall.js
@@ -0,0 +1,139 @@
+// Importing the react class for componnet building
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+// API calls 
+import axios from 'axios';
+
+// Loading the API constants 
+const host_site = process.env.REACT_APP_HOST
+const port = process.env.REACT_APP_BACKEND_PORT
+const username = process.env.REACT_APP_USERNAME
+const password = process.env.REACT_APP_PASSWORD
+
+// Defining the form class 
+class APIcall extends React.Component {
+
+    state = {
+        username: username,
+        password: password,
+        heartRate: null, 
+        glucose: null,
+        BMI: null,
+        totChol: null, 
+        cigsPerDay: null, 
+        age: null,
+        p: null
+    }
+
+    getProb = () => {
+        const APIurl = 'http://' + host_site + ':' + port + '/v2/predict';
+        axios({
+            url: APIurl, 
+            method: 'get', 
+            params: this.state
+        }).then(
+            //res => console.log(res)
+            res => this.setState({p: Number(res.data.probability.toFixed(3))})
+        )
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+          <div>
+            <div >
+            {this.state.p ? 
+                <div className='p-results'> 
+                    Probablity of heart failure in 10 years: 
+                    
+                    <br></br>
+                    
+                    {this.state.p} 
+                </div>
+                    : 
+                    <div> </div>
+            }
+            </div>
+
+            <Form>
+                
+                <Form.Group controlID='heartRate'>
+                    <Form.Label> Heart rate </Form.Label>
+                    <br />
+                    <Form.Control
+                        type='number'
+                        placeholder="Heart rate"
+                        value={this.state.heartRate}
+                        onChange={e => this.setState({ heartRate: e.target.value })}
+                    />
+                </Form.Group>
+
+                <Form.Group >
+                    <Form.Label> Glucose level </Form.Label>
+                    <br />
+                    <Form.Control
+                        type='number'
+                        placeholder="Glucose"
+                        value={this.state.glucose}
+                        onChange={e => this.setState({ glucose: e.target.value })}
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label> Body Mass Index (BMI) </Form.Label>
+                    <br />
+                    <Form.Control
+                        type='number'
+                        placeholder="BMI"
+                        value={this.state.BMI}
+                        onChange={e => this.setState({ BMI: e.target.value })}
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label> Total cholesterol </Form.Label>
+                    <br />
+                    <Form.Control
+                        type='number'
+                        placeholder="Cholesterol"
+                        value={this.state.totChol}
+                        onChange={e => this.setState({ totChol: e.target.value })}
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label> Cigarettes per day </Form.Label>
+                    <br />
+                    <Form.Control
+                        type='number'
+                        placeholder="Cigs per day"
+                        value={this.state.cigsPerDay}
+                        onChange={e => this.setState({ cigsPerDay: e.target.value })}
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label> Age </Form.Label>
+                    <br />
+                    <Form.Control
+                        type='number'
+                        placeholder="Age"
+                        value={this.state.age}
+                        onChange={e => this.setState({ age: e.target.value })}
+                    />
+                </Form.Group>
+
+                <div className='submit-user'>
+                    <Button onClick={this.getProb}>
+                        Get probability
+                    </Button>
+                </div>
+            </Form>
+        </div>     
+        )
+    }
+}
+
+export default APIcall;
